d2b9ffff34dd56088afb25a9ff3068c6c9b3dd43
sarakha63 <sarakha_ludovic@yahoo.fr>
2020-11-27 11:07:20 +0100

    prepare to stable

--- desktop/js/wiserlink.js
+++ desktop/js/wiserlink.js
@@ -14,6 +14,10 @@
  * You should have received a copy of the GNU General Public License
  * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
  */
+   $('#bt_resetSearch').off('click').on('click', function () {
+     $('#in_searchEqlogic').val('')
+     $('#in_searchEqlogic').keyup();
+ })
  
  $('#bt_healthwiserlink').on('click', function () {
     $('#md_modal').dialog({title: "{{Santé wiserlink}}"});
